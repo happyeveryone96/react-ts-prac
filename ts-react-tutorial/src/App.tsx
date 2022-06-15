9c2afe39a32ea0a80eeee1f4b9087787d786cb57
happyeveryone96 <happyeveryone@naver.com>
2022-06-15 15:47:10 +0900

    add: MyForm 컴포넌트 추가

--- ts-react-tutorial/src/App.tsx
+++ ts-react-tutorial/src/App.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import Greetings from "./Greetings";
 import Counter from "./Counter";
+import MyForm from "./MyForm";
 
 const App: React.FC = () => {
   const onClick = (name: string) => {
     console.log(`${name} says hello`);
   };
+  const onSubmit = (form: { name: string; description: string }) => {
+    console.log(form);
+  };
   return (
     <>
       <Greetings name="React" onClick={onClick} />
       <Counter />
+      <MyForm onSubmit={onSubmit} />
     </>
   );
 };
