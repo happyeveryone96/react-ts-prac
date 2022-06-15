f68e58c93870be274e43cb09fcfa32516bd89d63
happyeveryone96 <happyeveryone@naver.com>
2022-06-15 15:26:08 +0900

    add: Counter 컴포넌트 추가

--- ts-react-tutorial/src/App.tsx
+++ ts-react-tutorial/src/App.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import Greetings from "./Greetings";
+import Counter from "./Counter";
 
 const App: React.FC = () => {
   const onClick = (name: string) => {
     console.log(`${name} says hello`);
   };
-  return <Greetings name="React" onClick={onClick} />;
+  return (
+    <>
+      <Greetings name="React" onClick={onClick} />
+      <Counter />
+    </>
+  );
 };
 
 export default App;
