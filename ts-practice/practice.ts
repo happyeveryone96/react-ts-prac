90f5ca62864c2371fe332736470ededfc88efedf
happyeveryone96 <happyeveryone@naver.com>
2022-06-15 13:59:46 +0900

    Type Alias 사용하기

--- ts-practice/practice.ts
+++ ts-practice/practice.ts
@@ -1,25 +1,25 @@
-interface Person {
+type Person = {
   name: string;
-  age?: number; // 물음표가 들어갔다는 것은, 설정을 해도 되고 안해도 되는 값이라는 것을 의미
-}
+  age?: number;
+};
 
-// interface Developer {
-//   name: string;
-//   age?: number;
-//   skills: string[];
-// }
-
-// 상속
-interface Developer extends Person {
+// & 는 Intersection 으로서 두 개 이상의 타입들을 합쳐줌
+type Developer = Person & {
   skills: string[];
-}
+};
 
 const person: Person = {
   name: "김사람",
-  age: 20,
 };
 
 const expert: Developer = {
   name: "김개발",
   skills: ["javascript", "react"],
 };
+
+type People = Person[]; // Person[] 를 People 이라는 타입으로 사용 할 수 있음
+const people: People = [person, expert];
+
+type Color = "red" | "orange" | "yellow";
+const color: Color = "red";
+const colors: Color[] = ["red", "orange"];
