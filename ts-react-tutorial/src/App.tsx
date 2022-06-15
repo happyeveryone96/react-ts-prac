cb0c549304a18169bec6a20bf1fb0cf19b4ad642
happyeveryone96 <happyeveryone@naver.com>
2022-06-15 15:08:27 +0900

    add: greetings 컴포넌트 추가

--- ts-react-tutorial/src/App.tsx
+++ ts-react-tutorial/src/App.tsx
@@ -1,7 +1,11 @@
 import React from "react";
+import Greetings from "./Greetings";
 
 const App: React.FC = () => {
-  return <div className="App">hello</div>;
+  const onClick = (name: string) => {
+    console.log(`${name} says hello`);
+  };
+  return <Greetings name="React" onClick={onClick} />;
 };
 
 export default App;
