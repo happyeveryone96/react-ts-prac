29c677d3234f870de7e1210052d7013f2135b862
happyeveryone96 <happyeveryone@naver.com>
2022-06-15 10:19:34 +0900

    기본 타입

--- ts-practice/practice.ts
+++ ts-practice/practice.ts
@@ -1,2 +1,19 @@
-const message: string = "hello world";
-console.log(message);
+let count = 0; // 숫자
+count += 1;
+// count = "문자열"; // 에러
+
+const message: string = "hello world"; // 문자열
+
+const done: boolean = true; // 불리언 값
+
+const numbers: number[] = [1, 2, 3]; // 숫자 배열
+const messages: string[] = ["hello", "world"]; // 문자열 배열
+
+// messages.push(1); // 에러 ... 문자열 배열에 숫자 추가 불가능
+
+let mightBeUndefined: string | undefined = undefined; // string 또는 undefined
+let nullableNumber: number | null = null; // number 또는 null
+
+let color: "red" | "orange" | "yellow" = "red"; // red, orange, yellow 중 하나
+color = "yellow";
+// color = "green"; // 에러
