a7f06eb402b4b66e37c14fcf78917ca659d0cc93
happyeveryone96 <happyeveryone@naver.com>
2022-06-15 14:09:22 +0900

    interface 에서 Generics 사용하기

--- ts-practice/dist/practice.js
+++ ts-practice/dist/practice.js
@@ -1,22 +1,7 @@
 "use strict";
-// 제너릭(Generics)
-// 객체 A와 객체 B를 합쳐주는 merge 함수
-// A와 B가 어떤 타입이 올 지 모르기 때문에 제너릭을 사용한다.
-function merge(a, b) {
-    return Object.assign(Object.assign({}, a), b);
-}
-const merged = merge({ foo: 1 }, { bar: 1 });
-// any라는 타입을 쓸 수도 있지만 타입 유추가 모두 깨진거나 다름 없음
-// function merge(a: any, b: any): any {
-//   return {
-//     ...a,
-//     ...b
-//   };
-// }
-// const merged = merge({ foo: 1 }, { bar: 1 });
-function wrap(param) {
-    return {
-        param,
-    };
-}
-const wrapped = wrap(10);
+const items = {
+    list: ["a", "b", "c"],
+};
+const items2 = {
+    list: [1, 2, 3],
+};
--- ts-practice/practice.ts
+++ ts-practice/practice.ts
@@ -1,29 +1,11 @@
-// 제너릭(Generics)
-// 객체 A와 객체 B를 합쳐주는 merge 함수
-// A와 B가 어떤 타입이 올 지 모르기 때문에 제너릭을 사용한다.
-function merge<A, B>(a: A, b: B): A & B {
-  return {
-    ...a,
-    ...b,
-  };
+interface Items<T> {
+  list: T[];
 }
 
-const merged = merge({ foo: 1 }, { bar: 1 });
+const items: Items<string> = {
+  list: ["a", "b", "c"],
+};
 
-// any라는 타입을 쓸 수도 있지만 타입 유추가 모두 깨진거나 다름 없음
-// function merge(a: any, b: any): any {
-//   return {
-//     ...a,
-//     ...b
-//   };
-// }
-
-// const merged = merge({ foo: 1 }, { bar: 1 });
-
-function wrap<T>(param: T) {
-  return {
-    param,
-  };
-}
-
-const wrapped = wrap(10);
+const items2: Items<number> = {
+  list: [1, 2, 3],
+};
