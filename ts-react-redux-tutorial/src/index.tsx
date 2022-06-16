e3b0e31a89d2072ba3e265f30c45c03d8e3cc1df
happyeveryone96 <happyeveryone@naver.com>
2022-06-16 18:19:41 +0900

    add: 리덕스 적용

--- ts-react-redux-tutorial/src/index.tsx
+++ ts-react-redux-tutorial/src/index.tsx
@@ -1,13 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "./modules";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(
-  <React.StrictMode>
+const store = configureStore({ reducer: { rootReducer } });
+
+ReactDOM.render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>,
+  document.getElementById("root")
 );
